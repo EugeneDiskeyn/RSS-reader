e791254c1344f9b717affd6157ea9a1587f4aec8
EugeneDiskeyn <jdorovko@gmail.com>
2024-08-02 23:10:48 +0200

    Added url validation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
-import "./styles.scss"
-console.log("Hello World!");
-const comp = document.createElement("p");
-comp.innerHTML = "New component";
+import "./styles.scss";
+import 'bootstrap';
+import { string } from 'yup';
+import onChange from "on-change";
+
+const state = { urls: [""] };
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const message = document.querySelector("p");
+
+const watchedObject = onChange(state, () => {
+    input.value = "";
+    input.focus();
+});
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const schema = string().url().notOneOf(watchedObject.urls);
+    schema.validate(input.value)
+    .then(() => {
+        message.innerHTML = "";
+        input.classList.remove("is-invalid");
+        watchedObject.urls.push(input.value);
+    })
+    .catch(() => {
+        message.innerHTML = "Invalid url";
+        input.classList.add("is-invalid");
+    });
+})
